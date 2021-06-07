7973fe9396ff64b65ca3d643159411744e9a6088
pranav-kanungo <85475185+pranav-kanungo@users.noreply.github.com>
2021-06-07 11:24:58 +0530

    partial

---
+++ partial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test obj = new test();
+           int a= obj.add(6, 7);
+            int b=obj.sub(7, 8);
+            Console.WriteLine("the values are" +"  " +a +"  " +b);
+
+        }
+    }
+    public partial class test
+    {
+        public int add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+    }
+    public partial class test
+    {
+        public int sub(int a, int b)
+        {
+        int c = a - b;
+        return c;
+        }
+    }
+}
